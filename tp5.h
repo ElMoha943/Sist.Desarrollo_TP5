8f96a94482dbfa4c68c410faf862fae691f6eabc
Valen <valentinodidio943@gmail.com>
2021-02-28 21:03:37 -0300

    Update tp5.h

--- tp5.h
+++ tp5.h
@@ -12,15 +12,11 @@
 #define D7 PORTCbits.RC7
 
 #define RS PORTBbits.RB7
-#define RW PORTBbits.RB6
 #define E PORTBbits.RB5
-
-//VARIABLES
 
 //FUNCIONES
 void InicializarDisplay(void);
 void BorrarDisplay(void);
-//void Mostrar(char *str, unsigned char pos, unsigned char linea);
 void Posicionar(unsigned char pos, unsigned char linea);
 
 #endif	/* TP5_H */
